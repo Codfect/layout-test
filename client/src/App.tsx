cec7bf4e59834bdc3ef593c2b37219cb7f965c3f
Oliver <oliverjgodoy@gmail.com>
2021-04-07 23:58:00 -0300

    Aside component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
+import { AsideMenu } from "./components/AsideMenu";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <>
+      <AsideMenu></AsideMenu>
       <Header></Header>
       <GlobalStyle />
     </>
---
+++ client/src/components/AsideMenu/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from "./styles";
+
+
+export function AsideMenu() {
+  return(
+    <Container>
+      aside
+    </Container>
+  );
+}
